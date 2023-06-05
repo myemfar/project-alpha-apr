af51230817bc0bd2010984be9fba1d305d3acabb
Bryan Fischer <myemfar@gmail.com>
2023-06-05 15:44:35 -0600

    Feature 7 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("", lambda req: redirect("/projects/"), name="home"),
+    path("accounts/", include("accounts.urls"))
 ]
