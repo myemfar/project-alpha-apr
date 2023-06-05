556e7652835ffa4bcddcc165b271292ef0b727d6
Bryan Fischer <myemfar@gmail.com>
2023-06-05 15:37:25 -0600

    Feature 6 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("", lambda req: redirect("/projects/"), name="home"),
 ]
