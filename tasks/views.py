a7f34b1a305617717812c7ce85070f2a1f8fec09
Bryan Fischer <myemfar@gmail.com>
2023-06-07 10:37:50 -0600

    Feature 16 complete

--- tasks/views.py
+++ tasks/views.py
@@ -18,3 +18,12 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create.html", context)
+
+
+@login_required
+def show_my_tasks(request):
+    task = Task.objects.filter(assignee=request.user)
+    context = {
+        "task": task,
+    }
+    return render(request, "tasks/list.html", context)
